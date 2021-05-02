56044a9eedc38442ef2c5971e79ce357e258673b
Krishnan Shankar <krishnans2006@gmail.com>
2021-05-01 20:02:52 -0400

    GCP

--- server.py
+++ server.py
@@ -2,7 +2,13 @@ from flask import Flask, jsonify, request
 from models import Trie
 
 app = Flask(__name__)
-trie = Trie()
+trie = None
+
+@app.before_first_request
+def setup_trie():
+    global trie
+    trie = Trie()
+
 
 @app.route('/')
 def index():
@@ -10,43 +16,54 @@ def index():
         "Message": "Welcome to the Trie API!"
     })
 
+
 @app.route("/display")
 def display():
+    global trie
     return jsonify({
         "Result": trie.display()
     })
 
+
 @app.route("/add", methods=["POST"])
 def add():
+    global trie
     key = request.form.get("key")
     trie.add(key)
     return jsonify({
         "Result": "Success"
     })
 
+
 @app.route("/delete", methods=["POST"])
 def delete():
+    global trie
     key = request.form.get("key")
     trie.delete(key)
     return jsonify({
         "Result": "Success"
     })
 
+
 @app.route("/search", methods=["POST"])
 def search():
+    global trie
     key = request.form.get("key")
     response = trie.search(key)
     return jsonify({
         "Result": response
     })
 
+
 @app.route("/autocomplete", methods=["POST"])
 def autocomplete():
+    global trie
     key = request.form.get("key")
     response = trie.autocomplete(key)
     return jsonify({
         "Result": response
     })
+
 
 if __name__ == '__main__':
     app.run()
